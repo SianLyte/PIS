fd2f4edd09829bb87a21d028e79579b1734c110e
Joleg1c-c <Joleg1c-c@mail.ru>
2023-11-30 02:00:45 +0500

    Доделанная история

--- GrpcClient_PI_21-01/Views/History.cs
+++ GrpcClient_PI_21-01/Views/History.cs
@@ -13,57 +13,96 @@ namespace GrpcClient_PI_21_01.Views
     public partial class HistoryForm : Form
     {
         private int _i = 0;
-        // нужно чтобы работало оно:
+        private List<OperationReply> _data;
+
         public HistoryForm(List<OperationReply> data)
         {
             InitializeComponent();
-            ParceData(data);
+            _data = data;
+            ParceData();
         }
 
         // а вот это убрать:
-        public HistoryForm(List<string> oldFormatData)
-        {
-            InitializeComponent();
-            ParceData(oldFormatData);
-        }
+        //public HistoryForm(List<string> oldFormatData)
+        //{
+        //    InitializeComponent();
+        //    ParceData(oldFormatData);
+        //}
 
         // подсказка: чтобы посмотреть какие есть свойства у OperationReply, просто напиши data[0].
         // и Visual Studio сам подскажет какие существуют, удачи!
-        private void ParceData(List<string> data)
+        //private void ParceData(List<string> data)
+        //{
+        //    surnameTextBox.Text = data[0].ToString();
+        //    nameTextBox.Text = data[1].ToString();
+        //    patronymicTextBox.Text = data[2].ToString();
+        //    //numberTextBox.Text = data[3].ToString(); // убрать: у нас в лабах нет поля телефон у пользователя
+        //    //mailTextBox.Text = data[4].ToString(); // убрать: у нас в лабах нет поля почта у пользователя
+        //    OrganisationTextBox.Text = data[5].ToString();
+        //    //nameStructureTextBox.Text = data[6].ToString(); // убрать: такого поля у организации нет
+        //    postTextBox.Text = data[7].ToString();
+        //    //trueNumberTextBox.Text = data[8].ToString(); // убрать: нет и не надо такого поля у юзера
+        //    //trueMailTextBox.Text = data[9].ToString(); // убрать: нет и не надо такого поля у юзера
+        //    loginTextBox.Text = data[10].ToString();
+        //    dateTimeTextBox.Text = data[11].ToString();
+        //    objectNumberTextBox.Text = data[12].ToString();
+        //    descriptionTextBox.Text = data[13].ToString(); // подкорректировать: замени на "вид действия"
+        //                                                   // и используй operation.ActionType
+        //    nameTableBox.Text = data[14].ToString(); // убрать: файлы мы не загружаем в нашей системе
+        //    // добавить: наименование таблицы, в которой произошло изменение
+        //}
+
+        private void ParceData()
         {
-            surnameTextBox.Text = data[0].ToString();
-            nameTextBox.Text = data[1].ToString();
-            patronymicTextBox.Text = data[2].ToString();
-            //numberTextBox.Text = data[3].ToString(); // убрать: у нас в лабах нет поля телефон у пользователя
-            //mailTextBox.Text = data[4].ToString(); // убрать: у нас в лабах нет поля почта у пользователя
-            OrganisationTextBox.Text = data[5].ToString();
-            //nameStructureTextBox.Text = data[6].ToString(); // убрать: такого поля у организации нет
-            postTextBox.Text = data[7].ToString();
-            //trueNumberTextBox.Text = data[8].ToString(); // убрать: нет и не надо такого поля у юзера
-            //trueMailTextBox.Text = data[9].ToString(); // убрать: нет и не надо такого поля у юзера
-            loginTextBox.Text = data[10].ToString();
-            dateTimeTextBox.Text = data[11].ToString();
-            objectNumberTextBox.Text = data[12].ToString();
-            descriptionTextBox.Text = data[13].ToString(); // подкорректировать: замени на "вид действия"
-                                                           // и используй operation.ActionType
-            nameTableBox.Text = data[14].ToString(); // убрать: файлы мы не загружаем в нашей системе
-            // добавить: наименование таблицы, в которой произошло изменение
+            var thisPage = _data[_data.Count - _i - 1];
+
+            surnameTextBox.Text = thisPage.User.Surname.ToString();
+            nameTextBox.Text = thisPage.User.Name.ToString();
+            patronymicTextBox.Text = thisPage.User.Patronymic.ToString();
+            OrganisationTextBox.Text = thisPage.User.Organization.Name.ToString();
+            postTextBox.Text = thisPage.User.Organization.Status.ToString();
+            loginTextBox.Text = thisPage.User.Login.ToString();
+            //dateTimeTextBox.Text = thisPage.Date.ToString(); // дата Null
+            objectNumberTextBox.Text = thisPage.ModifiedObjectId.ToString();
+            descriptionTextBox.Text = thisPage.Action.ToString();
+            //                                               // и используй operation.ActionType
+            nameTableBox.Text = thisPage.ModifiedTableName.ToString();
         }
 
-        private void ParceData(List<OperationReply> data)
+        private void next_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(data[_i].ToString());
-            //surnameTextBox.Text = data[0].ToString();
-            //nameTextBox.Text = data[1].ToString();
-            //patronymicTextBox.Text = data[2].ToString();
-            //OrganisationTextBox.Text = data[5].ToString();
-            //postTextBox.Text = data[7].ToString();
-            //loginTextBox.Text = data[10].ToString();
-            //dateTimeTextBox.Text = data[11].ToString();
-            //objectNumberTextBox.Text = data[12].ToString();
-            //descriptionTextBox.Text = data[13].ToString(); 
-            //                                               // и используй operation.ActionType
-            //nameTableBox.Text = data[14].ToString(); 
+            _i++;
+            CheckButtons();
+            ParceData();
+        }
+
+
+        private void privious_Click(object sender, EventArgs e)
+        {
+            _i--;
+            CheckButtons();
+            ParceData();
+        }
+
+        private void CheckButtons()
+        {
+            if (_i == 0)
+            {
+                priviousButton.Enabled = false;
+            }
+            else
+            {
+                priviousButton.Enabled = true;
+            }
+
+            if (_i == _data.Count - 1)
+            {
+                nextButton.Enabled = false;
+            }
+            else
+            {
+                nextButton.Enabled = true;
+            }
         }
     }
 }
