2ff7bdac423199763eb8cb753f012cb8d61314e5
Joleg1c-c <57722719+Joleg1c-c@users.noreply.github.com>
2023-12-17 17:32:36 +0500

    Update design all filters

--- GrpcClient_PI_21-01/Views/Filters/OrgFilter.Designer.cs
+++ GrpcClient_PI_21-01/Views/Filters/OrgFilter.Designer.cs
@@ -28,161 +28,179 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.regBox = new System.Windows.Forms.GroupBox();
-            this.regComboBox = new System.Windows.Forms.ComboBox();
-            this.typeComboBox = new System.Windows.Forms.ComboBox();
-            this.typeBox = new System.Windows.Forms.GroupBox();
-            this.statusBox = new System.Windows.Forms.GroupBox();
-            this.statusJuridical = new System.Windows.Forms.RadioButton();
-            this.statusIndividual = new System.Windows.Forms.RadioButton();
-            this.statusAll = new System.Windows.Forms.RadioButton();
-            this.close = new System.Windows.Forms.Button();
-            this.reset = new System.Windows.Forms.Button();
-            this.apply = new System.Windows.Forms.Button();
-            this.regBox.SuspendLayout();
-            this.typeBox.SuspendLayout();
-            this.statusBox.SuspendLayout();
-            this.SuspendLayout();
+            regBox=new GroupBox();
+            regComboBox=new ComboBox();
+            typeComboBox=new ComboBox();
+            typeBox=new GroupBox();
+            statusBox=new GroupBox();
+            statusAll=new RadioButton();
+            statusIndividual=new RadioButton();
+            statusJuridical=new RadioButton();
+            close=new Button();
+            reset=new Button();
+            apply=new Button();
+            regBox.SuspendLayout();
+            typeBox.SuspendLayout();
+            statusBox.SuspendLayout();
+            SuspendLayout();
             // 
             // regBox
             // 
-            this.regBox.Controls.Add(this.regComboBox);
-            this.regBox.Location = new System.Drawing.Point(12, 12);
-            this.regBox.Name = "regBox";
-            this.regBox.Size = new System.Drawing.Size(624, 91);
-            this.regBox.TabIndex = 0;
-            this.regBox.TabStop = false;
-            this.regBox.Text = "По адресу регистрации";
+            regBox.BackColor=Color.Wheat;
+            regBox.Controls.Add(regComboBox);
+            regBox.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            regBox.Location=new Point(12, 12);
+            regBox.Name="regBox";
+            regBox.Size=new Size(624, 91);
+            regBox.TabIndex=0;
+            regBox.TabStop=false;
+            regBox.Text="По адресу регистрации";
             // 
             // regComboBox
             // 
-            this.regComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.regComboBox.FormattingEnabled = true;
-            this.regComboBox.Location = new System.Drawing.Point(17, 35);
-            this.regComboBox.Name = "regComboBox";
-            this.regComboBox.Size = new System.Drawing.Size(586, 28);
-            this.regComboBox.TabIndex = 0;
+            regComboBox.BackColor=Color.OldLace;
+            regComboBox.DropDownStyle=ComboBoxStyle.DropDownList;
+            regComboBox.FlatStyle=FlatStyle.Popup;
+            regComboBox.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            regComboBox.FormattingEnabled=true;
+            regComboBox.Location=new Point(17, 35);
+            regComboBox.Name="regComboBox";
+            regComboBox.Size=new Size(586, 38);
+            regComboBox.TabIndex=0;
             // 
             // typeComboBox
             // 
-            this.typeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.typeComboBox.FormattingEnabled = true;
-            this.typeComboBox.Location = new System.Drawing.Point(17, 35);
-            this.typeComboBox.Name = "typeComboBox";
-            this.typeComboBox.Size = new System.Drawing.Size(586, 28);
-            this.typeComboBox.TabIndex = 0;
+            typeComboBox.BackColor=Color.OldLace;
+            typeComboBox.DropDownStyle=ComboBoxStyle.DropDownList;
+            typeComboBox.FlatStyle=FlatStyle.Popup;
+            typeComboBox.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            typeComboBox.FormattingEnabled=true;
+            typeComboBox.Location=new Point(17, 35);
+            typeComboBox.Name="typeComboBox";
+            typeComboBox.Size=new Size(586, 38);
+            typeComboBox.TabIndex=0;
             // 
             // typeBox
             // 
-            this.typeBox.Controls.Add(this.typeComboBox);
-            this.typeBox.Location = new System.Drawing.Point(12, 119);
-            this.typeBox.Name = "typeBox";
-            this.typeBox.Size = new System.Drawing.Size(624, 91);
-            this.typeBox.TabIndex = 1;
-            this.typeBox.TabStop = false;
-            this.typeBox.Text = "По типу организации";
+            typeBox.BackColor=Color.Wheat;
+            typeBox.Controls.Add(typeComboBox);
+            typeBox.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            typeBox.Location=new Point(12, 132);
+            typeBox.Name="typeBox";
+            typeBox.Size=new Size(624, 91);
+            typeBox.TabIndex=1;
+            typeBox.TabStop=false;
+            typeBox.Text="По типу организации";
             // 
             // statusBox
             // 
-            this.statusBox.Controls.Add(this.statusAll);
-            this.statusBox.Controls.Add(this.statusIndividual);
-            this.statusBox.Controls.Add(this.statusJuridical);
-            this.statusBox.Location = new System.Drawing.Point(12, 229);
-            this.statusBox.Name = "statusBox";
-            this.statusBox.Size = new System.Drawing.Size(624, 132);
-            this.statusBox.TabIndex = 2;
-            this.statusBox.TabStop = false;
-            this.statusBox.Text = "По статусу";
-            // 
-            // statusJuridical
-            // 
-            this.statusJuridical.AutoSize = true;
-            this.statusJuridical.Location = new System.Drawing.Point(17, 56);
-            this.statusJuridical.Name = "statusJuridical";
-            this.statusJuridical.Size = new System.Drawing.Size(166, 24);
-            this.statusJuridical.TabIndex = 0;
-            this.statusJuridical.TabStop = true;
-            this.statusJuridical.Text = "Юридическое лицо";
-            this.statusJuridical.UseVisualStyleBackColor = true;
-            // 
-            // statusIndividual
-            // 
-            this.statusIndividual.AutoSize = true;
-            this.statusIndividual.Location = new System.Drawing.Point(17, 86);
-            this.statusIndividual.Name = "statusIndividual";
-            this.statusIndividual.Size = new System.Drawing.Size(284, 24);
-            this.statusIndividual.TabIndex = 1;
-            this.statusIndividual.TabStop = true;
-            this.statusIndividual.Text = "Индивидуальный предприниматель";
-            this.statusIndividual.UseVisualStyleBackColor = true;
+            statusBox.BackColor=Color.Wheat;
+            statusBox.Controls.Add(statusAll);
+            statusBox.Controls.Add(statusIndividual);
+            statusBox.Controls.Add(statusJuridical);
+            statusBox.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            statusBox.Location=new Point(12, 246);
+            statusBox.Name="statusBox";
+            statusBox.Size=new Size(624, 150);
+            statusBox.TabIndex=2;
+            statusBox.TabStop=false;
+            statusBox.Text="По статусу";
             // 
             // statusAll
             // 
-            this.statusAll.AutoSize = true;
-            this.statusAll.Location = new System.Drawing.Point(17, 26);
-            this.statusAll.Name = "statusAll";
-            this.statusAll.Size = new System.Drawing.Size(54, 24);
-            this.statusAll.TabIndex = 2;
-            this.statusAll.TabStop = true;
-            this.statusAll.Text = "Все";
-            this.statusAll.UseVisualStyleBackColor = true;
+            statusAll.AutoSize=true;
+            statusAll.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            statusAll.Location=new Point(17, 36);
+            statusAll.Name="statusAll";
+            statusAll.Size=new Size(65, 34);
+            statusAll.TabIndex=2;
+            statusAll.TabStop=true;
+            statusAll.Text="Все";
+            statusAll.UseVisualStyleBackColor=true;
+            // 
+            // statusIndividual
+            // 
+            statusIndividual.AutoSize=true;
+            statusIndividual.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            statusIndividual.Location=new Point(17, 110);
+            statusIndividual.Name="statusIndividual";
+            statusIndividual.Size=new Size(348, 34);
+            statusIndividual.TabIndex=1;
+            statusIndividual.TabStop=true;
+            statusIndividual.Text="Индивидуальный предприниматель";
+            statusIndividual.UseVisualStyleBackColor=true;
+            // 
+            // statusJuridical
+            // 
+            statusJuridical.AutoSize=true;
+            statusJuridical.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            statusJuridical.Location=new Point(17, 70);
+            statusJuridical.Name="statusJuridical";
+            statusJuridical.Size=new Size(194, 34);
+            statusJuridical.TabIndex=0;
+            statusJuridical.TabStop=true;
+            statusJuridical.Text="Юридическое лицо";
+            statusJuridical.UseVisualStyleBackColor=true;
             // 
             // close
             // 
-            this.close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
-            this.close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.close.Location = new System.Drawing.Point(495, 383);
-            this.close.Name = "close";
-            this.close.Size = new System.Drawing.Size(141, 29);
-            this.close.TabIndex = 16;
-            this.close.Text = "Закрыть";
-            this.close.UseVisualStyleBackColor = false;
+            close.BackColor=Color.Wheat;
+            close.FlatStyle=FlatStyle.Flat;
+            close.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            close.Location=new Point(495, 429);
+            close.Name="close";
+            close.Size=new Size(141, 40);
+            close.TabIndex=16;
+            close.Text="Закрыть";
+            close.UseVisualStyleBackColor=false;
             // 
             // reset
             // 
-            this.reset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
-            this.reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.reset.Location = new System.Drawing.Point(257, 383);
-            this.reset.Name = "reset";
-            this.reset.Size = new System.Drawing.Size(141, 29);
-            this.reset.TabIndex = 15;
-            this.reset.Text = "Сбросить";
-            this.reset.UseVisualStyleBackColor = false;
+            reset.BackColor=Color.Wheat;
+            reset.FlatStyle=FlatStyle.Flat;
+            reset.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            reset.Location=new Point(257, 429);
+            reset.Name="reset";
+            reset.Size=new Size(141, 40);
+            reset.TabIndex=15;
+            reset.Text="Сбросить";
+            reset.UseVisualStyleBackColor=false;
             // 
             // apply
             // 
-            this.apply.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
-            this.apply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.apply.Location = new System.Drawing.Point(12, 383);
-            this.apply.Name = "apply";
-            this.apply.Size = new System.Drawing.Size(141, 29);
-            this.apply.TabIndex = 14;
-            this.apply.Text = "Применить";
-            this.apply.UseVisualStyleBackColor = false;
+            apply.BackColor=Color.Wheat;
+            apply.FlatStyle=FlatStyle.Flat;
+            apply.Font=new Font("Segoe Print", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            apply.Location=new Point(12, 429);
+            apply.Name="apply";
+            apply.Size=new Size(141, 40);
+            apply.TabIndex=14;
+            apply.Text="Применить";
+            apply.UseVisualStyleBackColor=false;
             // 
             // OrgFilter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(648, 433);
-            this.Controls.Add(this.close);
-            this.Controls.Add(this.reset);
-            this.Controls.Add(this.apply);
-            this.Controls.Add(this.statusBox);
-            this.Controls.Add(this.typeBox);
-            this.Controls.Add(this.regBox);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.MaximizeBox = false;
-            this.Name = "OrgFilter";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Фильтры: Организации";
-            this.regBox.ResumeLayout(false);
-            this.typeBox.ResumeLayout(false);
-            this.statusBox.ResumeLayout(false);
-            this.statusBox.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions=new SizeF(8F, 20F);
+            AutoScaleMode=AutoScaleMode.Font;
+            BackColor=Color.Tan;
+            ClientSize=new Size(648, 489);
+            Controls.Add(close);
+            Controls.Add(reset);
+            Controls.Add(apply);
+            Controls.Add(statusBox);
+            Controls.Add(typeBox);
+            Controls.Add(regBox);
+            FormBorderStyle=FormBorderStyle.FixedSingle;
+            MaximizeBox=false;
+            Name="OrgFilter";
+            ShowIcon=false;
+            StartPosition=FormStartPosition.CenterParent;
+            Text="Фильтры: Организации";
+            regBox.ResumeLayout(false);
+            typeBox.ResumeLayout(false);
+            statusBox.ResumeLayout(false);
+            statusBox.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
